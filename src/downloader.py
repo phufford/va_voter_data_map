996159845875b78d24835ec397c89f3de84070f4
Marco Sirabella <msirabel@gmail.com>
2017-05-11 20:53:13 -0400

    - Moved to python3

--- src/downloader.py
+++ src/downloader.py
@@ -20,15 +20,16 @@ while election_id:
     election_id = ids.readline().strip() # Remove /n
     print('downloading: ' + election_id)
     try:
-        with open(DIRECTORY + election_id + '.csv', 'w') as csv:
+        with open(DIRECTORY + election_id + '.csv', 'wb') as csv:
             response = urllib.request.urlopen(build_url(election_id))
             csv.write(response.read())
-        with open(DIRECTORY + election_id + '_precinct' + '.csv', 'w') as csv:
+        with open(DIRECTORY + election_id + '_precinct' + '.csv', 'wb') as csv:
             response = urllib.request.urlopen(
                 build_url(election_id) + '/precincts_include:1/')
             csv.write(response.read())
 
-    except:
+    except Exception as e:
+        print(e)
         print ('Failed to download ' + election_id + '/n')
         with open(LOG_FILENAME, 'w') as log:
             log.write('Failed to download ' + election_id + '/n')
