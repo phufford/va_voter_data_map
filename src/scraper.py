c46622c56e4634117ed5c328f13ea32fd85496b3
Marco Sirabella <msirabel@gmail.com>
2017-05-24 14:44:37 -0400

    - Added logging

--- src/scraper.py
+++ src/scraper.py
@@ -6,6 +6,7 @@ import PIL.Image
 import urllib.request
 import io
 import os
+import logging
 
 __appname__     = ""
 __author__      = "Marco Sirabella"
@@ -21,6 +22,8 @@ __module__      = ""
 
 DRIVER_PATH = 'include/phantomjs/phantomjs'
 URL = 'http://historical.elections.virginia.gov'
+logger = logging.Logger('root')
+logging.basicConfig(level=logging.INFO)
 
 
 def debug(driver):
@@ -29,9 +32,13 @@ def debug(driver):
 
 if os.path.exists(DRIVER_PATH):
     browser = selenium.webdriver.PhantomJS(DRIVER_PATH)
+    logging.info('Using local installation of phantomjs')
 else:
     browser = selenium.webdriver.PhantomJS()
+    logging.info('Using default installation of phantomjs')
+
 browser.get(URL)
+logging.info(f'Going to page `{URL}`')
 
 findelement = browser.find_element_by_css_selector
 findelements = browser.find_elements_by_css_selector
@@ -39,12 +46,14 @@ findelements = browser.find_elements_by_css_selector
 browser.find_element_by_css_selector(
     '''#search_form_elections>div>#SearchIndexForm>div>input.splashy.tan'''
 ).click()
+logging.info(f'Clicking on search button to get all results')
 
 
 def curl(url):
     """
     http://stackoverflow.com/questions/7243750/download-file-from-web-in-python-3
     """
+    logger.debug(f'Downloading {url}')
     response = urllib.request.urlopen(url)
     data = response.read()
     return data.decode()
@@ -62,13 +71,26 @@ def getCSV(tablerow):
 
 
 def pages(driver):
-    while True:
+    old = False
+    new = True
+    while old != new:
+        print(old == new)
+        old = driver.find_element_by_css_selector('body').get_attribute(
+            'innerHTML'
+        )
+        logging.info('Fetching new page')
         driver.find_element_by_css_selector(
             '#search_results_table_next'
         ).click()
         yield driver
+        new = driver.find_element_by_css_selector('body').get_attribute(
+            'innerHTML'
+        )
 
 
 for i in pages(browser):
+    i.find_element_by_css_selector(
+        '#search_results_table_next'
+    ).get_attribute('class')
     for row in browser.find_elements_by_css_selector('tr.election_item'):
         getCSV(row)
