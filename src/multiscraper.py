904b7c268f68d96be7e4ae974e065f5d2f4f0df6
Marco Sirabella <msirabel@gmail.com>
2017-06-01 11:21:16 -0400

    - Added multiprocessing fixed

--- src/multiscraper.py
+++ src/multiscraper.py
@@ -3,6 +3,7 @@
 
 import os
 from scraper import *
+from selenium.webdriver.support.ui import Select
 import time
 
 __appname__     = ""
@@ -16,15 +17,47 @@ __email__       = "msirabel@gmail.com"
 __status__      = "Prototype"  # "Prototype", "Development" or "Production"
 __module__      = ""
 
-year = 2016
-f = os.fork()
-while f != 0 and year > 1900:
-    time.sleep(1)
-    year -= 1
-    f = os.fork()
+#year = 2016
+#f = os.fork()
+#while f != 0 and year > 1900:
+#    time.sleep(1)
+#    year -= 1
+#    f = os.fork()
 
+#valid_years = list(years(init()))
 browser = init()
-for year in years(browser, [str(year)]):
-    for i in pages(browser):
-        for row in browser.find_elements_by_css_selector('tr.election_item'):
-            getCSV(row)
+valid_years = [int(year.get_attribute('innerHTML')) for year in
+    browser.find_element_by_css_selector(
+        '#SearchYearFrom'
+    ).find_elements_by_css_selector('option')
+]
+def process_one_year(which_year):
+    driver = init()
+    for year in years(driver, which_year):
+        print(year)
+        for i in pages(driver):
+            print(i)
+            for row in browser.find_elements_by_css_selector('tr.election_item'):
+                getCSV(row)
+
+from multiprocessing import Pool
+import logging
+import timeit
+logger = logging.getLogger()
+timer = timeit.Timer()
+starttime = timer.timer()
+logger.setLevel(0)
+
+max_pool_count = 16
+{
+    8: 522.168079962008,
+    4: 678.5831731929939,
+    2: 904.9906527489948,
+    1 :1634.690616922002
+}
+p = Pool(max_pool_count)
+valid_years = valid_years
+pool_args = [valid_years[i::max_pool_count] for i in range(max_pool_count)]
+p.map(process_one_year, pool_args)
+endtime = timer.timer()
+logging.debug(f'time taken to process: {endtime - starttime}')
